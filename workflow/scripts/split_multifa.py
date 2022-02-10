edb3ac612af50a5e3d2382c70817b153fce986d2
ChrisgKent <chrisgkent@gmail.com>
2022-02-10 19:31:08 +0000

    Working first draft

--- workflow/scripts/split_multifa.py
+++ workflow/scripts/split_multifa.py
@@ -1,21 +1,21 @@
 from Bio import SeqIO
+import pathlib
 
 
 def main(
-    input_multifa_dir=snakemake.input[0],
-    OUTPUT=snakemake.output["out_dir"],
-    ORDER=snakemake.output["out_order"],
+    input_multifa_dir=snakemake.input[0], OUTPUT=snakemake.params[0],
 ):
     genome_data = [
         (record.id, record) for record in SeqIO.parse(input_multifa_dir, format="fasta")
     ]
+    # Generates the dir to store the referance
+    pathlib.Path(OUTPUT / "referance").mkdir(parents=True, exist_ok=True)
 
-    order = []
-    for genome in genome_data:
-        SeqIO.write(genome[1], f"{OUTPUT}/{genome[0]}.fasta", "fasta")
-        order.append(genome[0])
-
-    # Write the order file
+    for pos, genome in enumerate(genome_data):
+        if pos == 0:
+            SeqIO.write(genome[1], f"{OUTPUT}/referance/{genome[0]}.fasta", "fasta")
+        else:
+            SeqIO.write(genome[1], f"{OUTPUT}/{genome[0]}.fasta", "fasta")
 
 
 if __name__ == "__main__":
