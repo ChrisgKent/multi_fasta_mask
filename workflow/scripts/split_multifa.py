9c0dd156ce5d804caab7dcec4cd59da31f05a55a
ChrisgKent <chrisgkent@gmail.com>
2022-02-11 11:23:51 +0000

    Added a check to ensure that the mutli_fa contains more than one sequence

--- workflow/scripts/split_multifa.py
+++ workflow/scripts/split_multifa.py
@@ -8,9 +8,14 @@ def main(
     genome_data = [
         (record.id, record) for record in SeqIO.parse(input_multifa_dir, format="fasta")
     ]
+    # If there is only than one genome in fasta, exception is raised
+    if len(genome_data) <= 1:
+        raise Exception(
+            f"ERROR: {input_multifa_dir} does not contain more than one sequence"
+        )
+
     # Generates the dir to store the referance
     pathlib.Path(OUTPUT / "referance").mkdir(parents=True, exist_ok=True)
-
     for pos, genome in enumerate(genome_data):
         if pos == 0:
             SeqIO.write(genome[1], f"{OUTPUT}/REF_{genome[0]}.fasta", "fasta")
