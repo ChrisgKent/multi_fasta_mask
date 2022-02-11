83345b4358dec2c6d032cd055b80735216a901b9
ChrisgKent <chrisgkent@gmail.com>
2022-02-11 11:32:28 +0000

    Comment updates

--- workflow/scripts/split_multifa.py
+++ workflow/scripts/split_multifa.py
@@ -8,7 +8,7 @@ def main(
     genome_data = [
         (record.id, record) for record in SeqIO.parse(input_multifa_dir, format="fasta")
     ]
-    # If there is only than one genome in fasta, exception is raised
+    # If there is one or less genomes in fasta, exception is raised
     if len(genome_data) <= 1:
         raise Exception(
             f"ERROR: {input_multifa_dir} does not contain more than one sequence"
